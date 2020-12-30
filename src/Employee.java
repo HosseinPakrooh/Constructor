12b5b6836990b20d179b7e46fbed7f11945f3343
Hossein <www.jokerhpp@gmail.com>
2020-12-30 23:34:31 +0330

    first commit

---
+++ src/Employee.java
@@ -0,0 +1,13 @@
+ class Employee {
+	int num = 0;
+	
+	Employee(String s){
+		System.out.println(s);
+	}
+	
+	Employee(int i){
+		num = i + 5;
+		System.out.println(num);
+	}
+
+}
---
+++ src/testclass.java
@@ -0,0 +1,9 @@
+
+public class testclass {
+
+	public static void main(String[] args) {
+		Employee e = new Employee(15);
+		Employee e2 = new Employee("Ali");
+	}
+
+}
